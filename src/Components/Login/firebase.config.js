6b63914b666ce43b6ccd828ad4b206d1d4d919fd
Golamrabbani-ux <golamrabbani227417@gmail.com>
2021-01-31 01:17:32 +0600

    Page not found

---
+++ src/Components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCWKwt1R_MBrbFoInh_T5NY4EGTqjrji60",
+  authDomain: "creative-agency-grr.firebaseapp.com",
+  projectId: "creative-agency-grr",
+  storageBucket: "creative-agency-grr.appspot.com",
+  messagingSenderId: "536533615753",
+  appId: "1:536533615753:web:99a5419b59041d2d42f258",
+  measurementId: "G-TCSR24DQV1"
+};
+export default firebaseConfig;
